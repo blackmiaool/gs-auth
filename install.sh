31e5825732a5c1f31f67c9db47a027abf42cff73
blackmiaool <blackmiaool@gmail.com>
2022-07-27 11:04:15 +0800

    fix auth

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install python3-wxgtk4.0 
 pip3 install --user cryptography
-cp "./install/GS Auth.png" ~/launcher/skin/default/Menu/GameShell
+cp "./install/GS_Auth.png" ~/launcher/skin/default/Menu/GameShell
 cp "./install/11_GS_AUTH.sh" ~/launcher/Menu/GameShell
